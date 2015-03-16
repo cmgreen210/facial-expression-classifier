941f4d4a3815df01ec4484afc7b9b50bc42fa01d
Chris Green <cmgreen210@gmail.com>
2015-03-16 11:55:07 -0700

    rectangle fix

--- fec/media/image_processing.py
+++ fec/media/image_processing.py
@@ -50,7 +50,7 @@ class FaceDetectorProcessor(ImageProcessor):
 
     def __init__(self, cascade_file='haarcascade_frontalface_alt.xml',
                  return_largest=True, scale_x=1.5, scale_y=1.5, shape=None,
-                 add_rectangle=True, rect_color = (91,192,222)):
+                 add_rectangle=True, rect_color=(91, 192, 222)):
         self.detector = FaceDetector(cascade_file,
                                      return_largest=return_largest)
 
@@ -70,11 +70,11 @@ class FaceDetectorProcessor(ImageProcessor):
         if len(face) == 0:
             return image, None
         x, y, w, h = face[0]
-        w_new = w * self.scale_x
-        h_new = h * self.scale_y
+        w_new = int(floor(w * self.scale_x))
+        h_new = int(floor(h * self.scale_y))
 
-        y = max(y - floor((h_new - h)/2), 0)
-        x = max(x - floor((w_new - w)/2), 0)
+        y = int(max(y - floor((h_new - h)/2), 0))
+        x = int(max(x - floor((w_new - w)/2), 0))
 
         gray = gray[y:y+h_new, x:x+w_new]
         gray = self.postprocessor.process_image(gray)
@@ -89,3 +89,9 @@ class FaceDetectorProcessor(ImageProcessor):
 def run_face_detector(image):
     detector = FaceDetectorProcessor()
     detector.process_image(image)
+
+
+if __name__ == '__main__':
+    fd_proces = FaceDetectorProcessor()
+    img = cv2.imread('/Users/chris/face-emotion-classifier/media/ZoyNE2Ww5Z/1-frame.png')
+    fd_proces.process_image(img)
