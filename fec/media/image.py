5c340f972cd16b89e3083120b533529ecfb908a1
Chris Green <cmgreen210@gmail.com>
2015-03-18 08:06:59 -0700

    fixing bug

--- fec/media/image.py
+++ fec/media/image.py
@@ -45,6 +45,6 @@ class ImageFileClassifier(object):
         clf_image = face_arr.pixel_array_to_image(h, w, channels,
                                                   allow_rounding=True)
         # x = gl.SFrame({'images': clf_image})
-        x = np.expend_dims(face, axis=0)
+        x = np.expand_dims(face, axis=0)
         classifications = self._classifier(x)
         return image, clf_image[0], classifications
